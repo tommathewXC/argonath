e927a30034a3f908e44153dee961b95ce9d0abd3
tommathewXC <tomsmat1990@gmail.com>
2019-05-26 01:58:48 -0400

    Added some definite signs that its using C++ STL

--- cpp-extension/src/utils.cpp
+++ cpp-extension/src/utils.cpp
@@ -2,8 +2,16 @@
 #include "../headers/utils.h"
 
 #include <iostream>
+#include <unordered_map>
 
 int __internalAdder( const int * r1, const int * r2 ){
     std::cout << "C++ output" << std::endl;
+    std::unordered_map<std::string, std::string> kv;
+    kv["itema"] = "ayo this is an unorderd map";
+    kv["itemb"] = "damn it feels good to be a gangsta";
+    for( auto kvp: kv ){
+        std::cout << "Key: " << kvp.first << ", Value: " << kvp.second << std::endl;
+    }
+
     return (*r1) + (*r2);
 }
